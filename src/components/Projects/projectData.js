0f58c7c1cc92aa60b8e8f3c9b4d1efdddfeb0754
Sholom Perman <sholomperman@gmail.com>
2024-01-30 22:27:43 -0500

    added project data and made the btn desabled if not link

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,6 +1,7 @@
 import Zmanim from "../../Assets/Projects/ZmanimBot.png";
 import boxes from "../../Assets/Projects/boxes.png";
 import remote from "../../Assets/Projects/remote.png";
+import mosaic from "../../Assets/Projects/mosaic.png";
 
 
 
@@ -34,5 +35,14 @@ export const ProjectsData = [
         lights into your smart home ecosystem using Alexa voice commands. Simply instruct Alexa,
         and watch as your ordinary light responds to infrared signals, transforming it into a smart,
         connected lighting solution. Experience the convenience of unified control and modernize your home effortlessly.`,
+        ghLink: "#"
+    },
+    {
+        id: 4,
+        imgPath: mosaic,
+        isBlog: false,
+        title: "Create a Mosaic",
+        description: `Mosaic Maker, crafting awesome mosaics in a snap!  Turn your photos into eye-catching mosaics without breaking a sweat.`,
+        ghLink: "https://github.com/sholomperman/Mosaic-generator"
     },
 ]
